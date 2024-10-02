bedf5e4b17dcfa100be0415d71580e3cc4d5a863
Robert Mack <robertmack123123@gmail.com>
2024-10-02 13:24:25 -0500

    cleaned up code and styled drop down menus

--- src/components/miniatures/Miniature.jsx
+++ src/components/miniatures/Miniature.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 import "./miniatures.css";
-// html to display miniature
+
 export const Miniature = ({ miniature, disableLink }) => {
   return (
     <div className="miniature-container">
       <h1>{miniature.name}</h1>
       <div>
-        {disableLink ? ( // doesn't allow the user to click on the photo if the miniature if on the details page
+        {disableLink ? (
           <img src={miniature.img_url} alt={miniature.name} />
         ) : (
-          // takes users to form to edit the currently selected miniature
           <Link to={`/vault/${miniature.id}`}>
             <img src={miniature.img_url} alt={miniature.name} />
           </Link>
